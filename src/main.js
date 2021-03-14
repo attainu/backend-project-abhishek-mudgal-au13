872a462eb2d9d7e0ce33737e03e3c56453765027
Abhishek Mudgal <abhishek.mudgal.59@gmail.com>
2021-03-15 05:21:32 +0530

    Lets setup UI for the website

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import {InitMongo} from './models/configs/mongo-config'
 import bodyParser, { urlencoded } from 'body-parser'
-import expressip from "express-ip";
+import expressip from "express-ip"
+import path from 'path'
+import exphbs from 'express-handlebars'
+
 
 // Importing API
 import {signup} from './API/signup'
@@ -11,14 +14,29 @@ import {redirect} from './API/redirect'
 
 
 InitMongo()
+exphbs()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+const layoutsDir = path.join(__dirname, "views/layouts")
+const partialsDir = path.join(__dirname, "views/partials")
+app.set('views', path.join(__dirname, 'views'))
+app.set('trust proxy', true)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.set('trust proxy', true)
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(expressip().getIpInfoMiddleware)
+
+app.engine('hbs', exphbs({
+    extname: '.hbs',
+    defaultLayout: "main",
+    layoutsDir: layoutsDir,
+    partialsDir: partialsDir,
+}))
+
+app.set('view engine', 'hbs');
+
 
 // app.use(useragent.express())
 
@@ -27,7 +45,7 @@ app.use('/api', login)
 app.use('/api', dashboard)
 app.use('/', redirect)
 app.get('/', (req, res) => {
-    res.redirect('https://skindle.herokuapp.com/9gf108')
+    res.render('index')
 })
 
 app.listen(PORT, ()=>{
